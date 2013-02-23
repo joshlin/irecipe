1e480c857dcf19aa39587dc54d3dbf5ee792c794
Josh Lin <joshlin@uw.edu>
2013-02-22 22:31:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irecipe::Application.config.secret_token = 'bd27bd73bbe581b13203f3233571629e179fabdb5b136229d6a94a00bf19bdda83e091a2beb20e845e98a7b6a81ec980435eab5da6c4fb883516ca4d421ad07b'
